b151038401917aa165a13a787d4aba345c96cdf2
programmer9176 <myeducation9176@gmail.com>
2026-01-20 02:39:37 +0500

    refactor boat rendering logic; implement renderBoats function and update boat-card partial

--- public/js/filters.js
+++ public/js/filters.js
@@ -40,7 +40,7 @@ function handleBrandClick(e) {
         .filter(item => item.checked)
         .map(item => item.value);
 
-        fetchedBoats()
+    fetchedBoats()
     console.log(selectedBrands)
 
 }
@@ -68,8 +68,34 @@ async function fetchedBoats() {
         } else {
             console.error('Boat fetching failed');
         }
-        
+
     } catch (error) {
         console.error('Error fetching boats:', error);
     }
+}
+
+function renderBoats(boats) {
+    const boatContainer = $('#boat-listings');
+    console.log('abdullah', boats.thumbnail_image);
+
+    const thumbnailImage = boats.thumbnail_image || '';
+    const boatTitle = boats.title || '';
+    const boat_price = boats.price || 'Call for Price';
+
+    const boatCards = boats.map((boat) => {
+        return `<div class="col-lg-4 boat-card">
+                    <div class="boats-image">
+                        <a href="/boat-details/${boat._id}">
+                            <img src="${boat.thumbnail_image}" alt="${boat.title}">
+                        </a>
+                    </div>
+                    <div class="boat-card-body">
+                        <h3 class="boat-card-title"><a href="/boat-details/${boat._id}">${boat.title}</a></h3>
+                        ${boat.price ? `<p class="boat-card-price">Price: ${boat.price}</p>` : 'Call For Price'}
+                    </div>
+                </div>
+                `;
+    }).join('');
+
+    boatContainer.html(boatCards);
 }
