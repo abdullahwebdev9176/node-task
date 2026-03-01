b9a18fa531614fe2f7b59c915248de7ef116a07e
Muhammad Abdullah <abdullah.webdev9176@gmail.com>
2026-03-01 16:53:59 +0500

    Revert "updated load more"
    This reverts commit d1d3477501119e369ce871d6c2534e7417daba01.

--- routes/index.js
+++ routes/index.js
@@ -43,9 +43,6 @@ router.get('/:page', async(req, res) => {
 
     console.log('year', minYear, 'max year', maxYear);
 
-    const totalPages = Math.ceil(totalBoats / settings.boat_limit);
-    const currentPage = parseInt(page) || 1;
-
     const styles = [...jQueryUIStyle(), ...getStyles()];
     const scripts = [...getJquery(), ...jQueryUIScript(), ...getFilter()];
 
@@ -63,8 +60,6 @@ router.get('/:page', async(req, res) => {
         minYear: minYear,
         maxYear: maxYear,
         pageUrl: page,
-        totalPages: totalPages,
-        currentPage: currentPage,
         style: styles,
         scripts: scripts
     });
